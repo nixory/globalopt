7f2a7af1f98fbd765e2d13557d8c5a711282caa9
nixory pc <nikitastepanovs777@gmail.com>
2022-08-05 18:53:14 +0300

    pre adaptive

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,17 @@
 // Custom Scripts
 // Custom scripts
+document.addEventListener('click', function (e) {
+    var map = document.querySelector('#map-wrap iframe')
+    if (e.target.id === 'map-wrap') {
+        map.style.pointerEvents = 'all'
+    } else {
+        map.style.pointerEvents = 'none'
+    }
+})
 
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".feedback__swipper__button-next",
+      prevEl: ".feedback__swipper__button-prev",
+    },
+  });
