7688d83f5c41d193d2f42574ea2011a1ac0c2d02
nixory pc <nikitastepanovs777@gmail.com>
2022-08-15 14:34:50 +0300

    modal window: completed

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -11,7 +11,18 @@ document.addEventListener('click', function (e) {
 
 var swiper = new Swiper(".mySwiper", {
     navigation: {
-      nextEl: ".feedback__swipper__button-next",
-      prevEl: ".feedback__swipper__button-prev",
+        nextEl: ".feedback__swipper__button-next",
+        prevEl: ".feedback__swipper__button-prev",
     },
-  });
+});
+
+$('.modal-menu').hide();
+$("#header-modal").click(function () {
+    $('.modal-menu').show();
+});
+
+
+
+$("#exit, #benefit-btn, #ourwork-btn, #price-btn, #pattern-btn, #reviews-btn, #contacts-btn").click(function () {
+    $('.modal-menu').hide();
+});
